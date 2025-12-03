027690cfd9cb1f8b942f9e63c571efbf77a4cd9b
Phisbol <stan.magallon@gmail.com>
2025-12-04 00:44:17 +0800

    feat: add summary section to Home page with statistics and safety percentage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,46 @@ function Home(){
     return(
         <div className="content-wrapper">
             <PageHeader></PageHeader>
+            <div className="summary-container">
+                <div className="summary-bar summary-line">
+                    <div className="summary-bar-desc">
+                        <div>
+                            <p>Households</p>
+                            <p>{"10"}</p>
+                        </div>
+                    </div>
+                    <div className="summary-bar-desc">
+                        <div>
+                            <p>Safe</p>
+                            <p>{"10"}</p>
+                        </div>
+                    </div>
+                    <div className="summary-bar-desc">
+                        <div>
+                            <p>Danger</p>
+                            <p>{"10"}</p>
+                        </div>
+                        
+                    </div>
+                    <div className="summary-bar-desc">
+                        <div>
+                            <p>Unverified</p>
+                            <p>{"10"}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="summary-bar smaller-bar">
+                    <div className="summary-bar-desc">
+                        <div>
+                            <p>Safety Percentage</p>
+                            <p>{"10%"}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="active-incident-container">
+                <div className="page-sub-header">Active Incident</div>
+            </div>
         </div>
     )
 
