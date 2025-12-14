ad89b87b832da9a500ea62b56b21524913def95a
Daed <gatsby.gatbonton21@gmail.com>
2025-12-14 15:38:50 +0800

    Imported ResiTrack logos to replace generic template

--- client/src/components/NavPage.jsx
+++ client/src/components/NavPage.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom"
-import Icon from '../assets/Icon.png'
+import Icon from '../assets/darkIcon01t.png'
 
 function NavPage(){
     const location = useLocation();
