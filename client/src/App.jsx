0b81712b6a81f42a7de3ea1cc867a12f3b23ce23
Phisbol <stan.magallon@gmail.com>
2025-12-17 16:03:57 +0800

    feat: add Maps page and update routing; enhance CurrentLocationMap styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import { Routes ,Route } from 'react-router-dom'
 import Home from './pages/Home.jsx';
 import NavPage from './components/NavPage.jsx';
 import NavMobile from './components/NavMobile.jsx';
-import { Routes ,Route } from 'react-router-dom'
+import Maps from './pages/Maps.jsx';
+
 
 import Login from './pages/Login.jsx';
 function App() {
@@ -15,7 +17,7 @@ function App() {
         
           <Routes>
               <Route path='/' element={<div className='wrapper'><div className='contentBody'><Home></Home></div></div>}></Route>
-              <Route path='/map' element={<div className='wrapper'><div className='contentBody'><Home></Home></div></div>}></Route>
+              <Route path='/map' element={<div className='wrapper'><div className='contentBody'><Maps></Maps></div></div>}></Route>
           </Routes>
         
         <NavMobile></NavMobile>
